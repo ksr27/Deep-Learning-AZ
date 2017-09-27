33be85bcaf5352234416118a2dbca42268eebd0d
StefanDraghiciTraining <draghicistefan1984@gmail.com>
2017-09-27 22:15:54 +0300

    rnn done

--- 2. RNN/rnn.py
+++ 2. RNN/rnn.py
@@ -34,3 +34,36 @@ regressor=Sequential()
 
 # Add input layer and LSTM layer
 regressor.add(LSTM(units=4, activation='sigmoid', input_shape=(None, 1)))
+regressor.add(Dense(units=1))
+
+# Compile the RNN
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+
+# Fit the regressor to training set
+regressor.fit(X_train, Y_train, batch_size=32, epochs=200)
+
+# Part 3 - Make preds and visualize rez
+
+# Get the real stock price for 2017
+test_set=pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price=test_set.iloc[:, 1:2].values
+
+# Get the predicted stock price in 2017
+inputs=real_stock_price
+inputs=sc.transform(inputs)
+inputs=np.reshape(inputs, (20, 1, 1))
+
+predicted_stock_price=regressor.predict(inputs)
+predicted_stock_price=sc.inverse_transform(predicted_stock_price)
+
+# Visualize rez
+plt.plot(real_stock_price, color='red', label='real stock price')
+plt.plot(predicted_stock_price, color='blue', label='predicted stock price')
+plt.show()
+
+
+
+
+
+
+
