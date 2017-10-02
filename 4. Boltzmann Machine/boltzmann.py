c14aae469b5c9766ef707e425ee93cb13927a0b9
devstefandraghici@gmail.com <devstefandraghici@gmail.com>
2017-10-02 18:15:11 +0300

    pytorch tensors conversion

--- 4. Boltzmann Machine/boltzmann.py
+++ 4. Boltzmann Machine/boltzmann.py
@@ -23,3 +23,34 @@ ratings=pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python',
 
 # Prepare the training set and test set
 training_set=pd.read_csv('ml-100k/u1.base', delimiter='\t')
+training_set=np.array(training_set, dtype='int')
+
+test_set=pd.read_csv('ml-100k/u1.test', delimiter='\t')
+test_set=np.array(test_set, dtype='int')
+
+# Get the total num of movies and users
+nb_users=int(max(max(training_set[:, 0]), max(test_set[:, 0])))
+nb_movies=int(max(max(training_set[:, 1]), max(test_set[:, 1])))
+
+# Convert the data into an array of users, movies and ratings
+def convert(data):
+    new_data=[]
+    for id_users in range(1, nb_users+1):
+        id_movies=data[:,1][data[:,0]==id_users]
+        id_ratings=data[:,2][data[:,0]==id_users]
+        ratings=np.zeros(nb_movies)
+        ratings[id_movies-1]=id_ratings
+        new_data.append(list(ratings))
+    return new_data
+
+training_set=convert(training_set)
+test_set=convert(test_set)
+
+# Convert the data into torch tensors
+training_set=torch.FloatTensor(training_set)
+test_set=torch.FloatTensor(test_set)
+
+
+
+
+
