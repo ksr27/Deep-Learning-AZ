0de01e5ca6828fb92a90e5447176fc51e4f0d766
StefanDraghiciTraining <draghicistefan1984@gmail.com>
2017-10-04 23:39:59 +0300

    autoencoder done

--- 5. AutoEncoders/autoencoder.py
+++ 5. AutoEncoders/autoencoder.py
@@ -72,6 +72,49 @@ sae=SAE()
 criterion=nn.MSELoss()
 optimizer=optim.RMSprop(sae.parameters(), lr=0.01, weight_decay=0.5)
 
+# Train the Autoencoder
+nb_epoch=100
+
+for epoch in range(1, nb_epoch+1):
+    train_loss=0
+    s=0.0
+    
+    for id_user in range(nb_users):
+        input=Variable(training_set[id_user]).unsqueeze(0)
+        target=input.clone()
+        
+        if torch.sum(target.data>0)>0:
+            output=sae(input)
+            target.require_grad=False
+            output[target==0]=0
+            loss=criterion(output, target)
+            mean_corrector=nb_movies/float(torch.sum(target.data>0)+1e-10)
+            loss.backward()
+            train_loss+=np.sqrt(loss.data[0]*mean_corrector)
+            s+=1.0
+            optimizer.step()
+            
+    print('epoch: '+str(epoch)+' - loss: '+str(train_loss/s))
+
+# Test the Autoencoder
+test_loss=0
+s=0.0
+
+for id_user in range(nb_users):
+    input=Variable(training_set[id_user]).unsqueeze(0)
+    target=Variable(test_set[id_user])
+    
+    if torch.sum(target.data>0)>0:
+        output=sae(input)
+        target.require_grad=False
+        output[target==0]=0
+        loss=criterion(output, target)
+        mean_corrector=nb_movies/float(torch.sum(target.data>0)+1e-10)
+        test_loss+=np.sqrt(loss.data[0]*mean_corrector)
+        s+=1.0
+        
+print('loss: '+str(test_loss/s))
+
 
 
 
